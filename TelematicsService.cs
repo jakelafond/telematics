0cb586457e8882e219b6cd587e9c648948b42dea
Jake LaFond <jakelafond@gmail.com>
2017-08-18 10:14:08 -0400

    serializing objects with json

--- TelematicsService.cs
+++ TelematicsService.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Telematics
 {
     public class TelematicsService
     {
+        JsonSerializer serializer = new JsonSerializer();
        public void Report(VehicleInfo vehicleInfo)
        {
             using (var writer = new StreamWriter(File.Open($"{vehicleInfo.VIN}.json", FileMode.OpenOrCreate)))
+            using (var jsonWriter = new JsonTextWriter(writer))
             {
-                writer.WriteLine(vehicleInfo.VIN);
-                writer.WriteLine(vehicleInfo.Odometer);
-                writer.WriteLine(vehicleInfo.Consumption);
-                writer.WriteLine(vehicleInfo.OdometerLastOilChange);
-                writer.WriteLine(vehicleInfo.EngineSize);
+
+                serializer.Serialize(writer,vehicleInfo);
+                // writer.WriteLine(vehicleInfo.VIN);
+                // writer.WriteLine(vehicleInfo.Odometer);
+                // writer.WriteLine(vehicleInfo.Consumption);
+                // writer.WriteLine(vehicleInfo.OdometerLastOilChange);
+                // writer.WriteLine(vehicleInfo.EngineSize);
             }
        }
     }
