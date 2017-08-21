983c4df8fc1a245f187f6b0bf194a4cf95c759ca
Jake LaFond <jakelafond@gmail.com>
2017-08-21 18:01:10 -0400

    got the html working

--- Telematics.cs
+++ Telematics.cs
@@ -23,12 +23,15 @@ namespace Telematics
             ///add stuff to a list here
             List<object> vehicleList = new List<object>();
             var totalOdometer = 0d;
-            var itemTemplate = $@"<table align='center' border='1'>
+            var totalConsumption = 0d;
+            var totalLastOilChange = 0d;
+            var totalEngineSize = 0d;
+            var itemTemplate = @"<table align='center' border='1'>
           <tr>
               <th>VIN</th><th>Odometer (miles)</th><th>Consumption (gallons)</th><th>Last Oil Change</th><th>Engine Size (liters)</th>
           </tr>
           <tr>
-              <td align='center'>{0}</td><td align='center'>{1}</td><td align='center'>{2}</td><td align='center'>{3}</td><td align='center'>{4}</td>
+              <td align='center'>{0}</td><td align='center'>{1:0.0}</td><td align='center'>{2:0.0}</td><td align='center'>{3:0.0}</td><td align='center'>{4:0.0}</td>
           </tr>
       </table>";
       var tableHtml = string.Empty;
@@ -40,10 +43,17 @@ namespace Telematics
                     var vehicleInfo2 = JsonConvert.DeserializeObject<VehicleInfo>(file.ReadToEnd());
                     vehicleList.Add(vehicleInfo2);
                     totalOdometer += vehicleInfo2.Odometer;
+                    totalConsumption += vehicleInfo2.Consumption;
+                    totalLastOilChange += vehicleInfo2.OdometerLastOilChange;
+                    totalEngineSize += vehicleInfo2.EngineSize;
                     tableHtml += string.Format($"{itemTemplate}",vehicleInfo2.VIN,vehicleInfo2.Odometer,vehicleInfo2.Consumption,vehicleInfo2.OdometerLastOilChange,vehicleInfo2.EngineSize);
+                    Console.WriteLine(vehicleInfo2.VIN);
                 }
             }
             var odometerAverage = totalOdometer/vehicleList.Count;
+            var consumptionAverage = totalConsumption / vehicleList.Count;
+            var oilChangeAverage = totalLastOilChange / vehicleList.Count;
+            var engineSizeAverage = totalEngineSize / vehicleList.Count;
             string html = $@"<html>
     <title>Vehicle Telematics Dashboard</title>
     <body>
@@ -53,7 +63,7 @@ namespace Telematics
               <th>Odometer (miles) |</th><th>Consumption (gallons) |</th><th>Last Oil Change |</th><th>Engine Size (liters)</th>
           </tr>
           <tr>
-              <td align='center'>{odometerAverage}</td><td align='center'>2</td><td align='center'>3</td align='center'><td align='center'>4</td>
+              <td align='center'>{odometerAverage:0.0}</td><td align='center'>{consumptionAverage:0.0}</td><td align='center'>{oilChangeAverage:0.0}</td align='center'><td align='center'>{engineSizeAverage:0.0}</td>
           </tr>
       </table>
       <h1 align='center'>History</h1>
